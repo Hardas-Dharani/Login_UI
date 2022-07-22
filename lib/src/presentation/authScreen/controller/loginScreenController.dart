a8df1760d378551ff07341b0e49feb0cb702b419
Hardas Dharni <51167679+Hardas-Dharani@users.noreply.github.com>
2022-07-23 00:25:27 +0500

    update

--- lib/src/presentation/authScreen/controller/loginScreenController.dart
+++ lib/src/presentation/authScreen/controller/loginScreenController.dart
@@ -10,38 +10,4 @@ class LoginScreenController extends GetxController {
   TextEditingController email = TextEditingController();
   TextEditingController pass = TextEditingController();
   final loginKey = GlobalKey<FormState>();
-  String? validateCode(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter valid code';
-    } else {
-      return null;
-    }
-  }
-
-  final visable = false.obs;
-  final visables = false.obs;
-  @override
-  void onReady() {
-    Future.delayed(const Duration(milliseconds: 750)).whenComplete(
-      () => visables.value = true,
-    );
-    Future.delayed(const Duration(milliseconds: 800)).whenComplete(
-      () => visable.value = true,
-    );
-
-    super.onReady();
-  }
-
-  showPassWord() {
-    isPasswordVisible.value = !isPasswordVisible.value;
-  }
-
-  // Login Controller by API
-  // Future<Map<String, dynamic>> loginAPIController() async {
-  // var loginresponse = await AuthREPO().login(codectr.text);
-  // return loginresponse;
-  // }
-
-  final rememberMe = false.obs;
-  ontoggle() => rememberMe(!rememberMe.value);
 }
